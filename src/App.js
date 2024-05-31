67c0ee34dd2a5935f4f3fa1448a403ab285b81a9
Jeromino <jerojournade1@gmail.com>
2024-05-31 10:57:32 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='main-container'>
+          <div className='container'>
+            <h1 className='title'>OverGrip Counter</h1>
+            <div className='counter-container'>
+              <button onClick={() => {setCount(count - 1)}} className='button substract-button'>-</button>
+              <span className='number'>{count}</span>
+              <button onClick={() => {setCount(count + 1)}} className='button add button'>+</button>
+            </div><br></br>
+            <span className='total'>Total ${count * 500}</span>
+          </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
